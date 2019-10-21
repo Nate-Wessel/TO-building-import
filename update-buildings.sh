e2c54580d30741525afe43488aa8890af8431774
Nate-Wessel <nate.wessel@mail.utoronto.ca>
2019-10-21 10:50:33 -0400

    adding scripts to get and process OSM data

---
+++ update-buildings.sh
@@ -0,0 +1,6 @@
+# get buildings data
+wget -O buildings.osm --post-file=building-query.txt https://overpass-api.de/api/interpreter
+# populate database
+osm2pgsql --slim --hstore-all -x --prefix tobuild --proj 4326 --style building.style -d osm buildings.osm
+# add/update stuff
+psql -d osm -f simplification.sql

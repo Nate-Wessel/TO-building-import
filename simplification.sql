2736949e90fc14ba560b36d540e529701c1fb9ed
Nate-Wessel <nate.wessel@mail.utoronto.ca>
2019-10-25 10:07:20 -0400

    simplified simplification

--- simplification.sql
+++ simplification.sql
@@ -1,13 +1,11 @@
 -- look into simplification issues in existing OSM buildings
-
-﻿ALTER TABLE tobuild_polygon 
+ALTER TABLE tobuild_polygon 
 	ADD COLUMN orig_node_count int,
-	ADD COLUMN simp_node_count int;
-
-ALTER TABLE tobuild_polygon ADD COLUMN centroid geometry(point,4326);
-UPDATE tobuild_polygon SET centroid = ST_Centroid(way);
+	ADD COLUMN simp_node_count int,
+	ADD COLUMN centroid geometry(point,4326);
 
 UPDATE tobuild_polygon SET 
+	centroid = ST_Centroid(way),
 	orig_node_count = ST_NPoints(way),
 	simp_node_count = ST_Npoints( ST_Simplify( ST_Transform(way,32617), 0.15) );
 

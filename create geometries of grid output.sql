0cd34dd3f03c35939a00d11de0f8fe7a930b16d3
Nate-Wessel <nate.wessel@mail.utoronto.ca>
2019-11-02 15:15:37 -0400

    fixes to task-grid process

---
+++ create geometries of grid output.sql
@@ -0,0 +1,25 @@
+﻿DROP TABLE IF EXISTS temptab;
+CREATE TABLE temptab (
+	x1 real,
+	x2 real,
+	y1 real,
+	y2 real,
+	depth int,
+	num int
+);
+COPY temptab FROM '/home/nate/test.csv' CSV HEADER;
+
+DROP TABLE IF EXISTS tempgrid;
+SELECT 
+	depth, 
+	num AS num_nodes,
+	ST_SetSRID( ST_MakePolygon( ST_MakeLine(
+		ARRAY[ST_MakePoint(x1,y1),
+		ST_MakePoint(x1,y2),
+		ST_MakePoint(x2,y2),
+		ST_MakePoint(x2,y1),
+		ST_MakePoint(x1,y1)]
+	) ), 3857)
+INTO tempgrid
+FROM temptab;
+ALTER TABLE tempgrid ADD COLUMN uid serial primary key;
--- get-weighted-centroids.sql
+++ get-weighted-centroids.sql
@@ -1,7 +1,7 @@
 ﻿COPY (
 	SELECT 
-		ST_X(ST_Transform(centroid,32617)) as x,
-		ST_Y(ST_Transform(centroid,32617)) AS y,
+		ST_X(ST_Transform(centroid,3857)) as x,
+		ST_Y(ST_Transform(centroid,3857)) AS y,
 		orig_node_count - simp_node_count AS w
 	FROM tobuild_polygon
 	WHERE orig_node_count - simp_node_count > 0

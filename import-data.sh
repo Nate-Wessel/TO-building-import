74b0bbc67793ff1d117fe56de8b931cd36833d1c
Nate Wessel <bike756@gmail.com>
2020-01-08 10:09:49 -0500

    avoid downloading duplicate ways

--- import-data.sh
+++ import-data.sh
@@ -2,4 +2,4 @@
 # and unpack the shapefile. The data ill be imported into PostGIS
 # for easier handling. Fill in the connection details below.
 
-ogr2ogr -f PostgreSQL PG:"dbname='' host='localhost' user='' password=''" odb_ontario.shp --config PG_USE_COPY YES -nlt MULTIPOLYGON
+ogr2ogr -f PostgreSQL PG:"dbname='osm' host='localhost' user='' password=''" ODB_Ontario/odb_ontario.shp --config PG_USE_COPY YES -nlt MULTIPOLYGON
